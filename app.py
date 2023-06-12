14148c2c643dff483a38808da2a40de915afbb6f
saulo-silv <vitorgabriel@gmail.com>
2023-06-11 23:26:15 -0300

    commit

--- app.py
+++ app.py
@@ -3,23 +3,54 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Fonte de Dados
-# https://www.kaggle.com/datasets/whenamancodes/student-performance
-
-st.set_page_config(page_title="Dashboard - Student Dataset", page_icon=":books:")
-
-st.sidebar.title("Configurações de Exibição")
+base = pd.read_csv('./sad.csv')
+st.write(base)
 
 st.title("Análise de Dados do Dataset de Estudantes")
 
-st.sidebar.subheader("Selecione o que deseja exibir")
-show_dataset = st.sidebar.checkbox("Dados do Dataset")
-show_dataset_description = st.sidebar.checkbox("Descrição do Dataset")
+st.title("Primeira")
+#Questão 1
+media_idade_GP = base.loc[base['school'] == 'GP', 'age'].mean()
+st.write("A média de idade dos alunos na escola GP é:", media_idade_GP)
 
-graph1_type = st.sidebar.selectbox("Gráfico 1: Selecione o tipo de gráfico", ("Barra", "Pizza", "Dispersão", "Histograma", "Boxplot"))
 
-media = sum(dados) / len(dados)
+#Questão 2
+moda_endereco_MS = base.loc[base['school'] == 'MS', 'address'].mode().values[0]
+st.write("A moda do endereço dos alunos na escola MS é:", moda_endereco_MS)
 
-st.line_chart(pd.Series(dados).rolling(window=3).mean())
-st.write(f"Média: {media}")
+#Questão 3
+mediana_tempo_viagem_GP = base.loc[base['school'] == 'GP', 'traveltime'].median()
+st.write("A mediana do tempo de viagem dos alunos que estudam na escola GP é:", mediana_tempo_viagem_GP)
+
+#Questão 4 
+desvio_padrao_idade_apoio_MS = base.loc[(base['school'] == 'MS') & (base['studytime'] == 'sim'), 'age'].std()
+st.write("O desvio padrão da idade dos alunos que têm apoio educacional extra na escola MS é:", desvio_padrao_idade_apoio_MS)
+
+#Questão 5 
+media_tempo_estudo_pais_separados_GP = base.loc[(base['school'] == 'GP') & (base['guardian'] == 'sim'), 'studytime'].mean()
+st.write("A média do tempo semanal de estudo dos alunos cujos pais estão separados na escola GP é:", media_tempo_estudo_pais_separados_GP)
+
+#Questão 6 
+moda_motivo_escolha_MS = base.loc[base['school'] == 'MS', 'address'].mode().values[0]
+st.write("A moda do motivo pelo qual os alunos escolheram a escola MS é:", moda_motivo_escolha_MS)
+
+#Questão 7
+mediana_num_faltas_GP = base.loc[base['school'] == 'GP', 'absences'].median()
+st.write("A mediana do número de faltas dos alunos que frequentam a escola GP é:", mediana_num_faltas_GP)
+
+#Questão 8
+desvio_padrao_saude_atividades_MS = base.loc[(base['school'] == 'MS') & (base['activities'] == 'sim'), 'health'].std()
+st.write("O desvio padrão do nível de saúde dos alunos que frequentam atividades extracurriculares na escola MS é:", desvio_padrao_saude_atividades_MS)
+
+#Questão 9
+alunos_cumpriram_horas = base.loc[base['freetime'] > 0].shape[0]
+st.write("O número de alunos que já cumpriram as horas extracurriculares é:", alunos_cumpriram_horas)
+
+#Questão 10
+moda_consumo_alcool_MS = base.loc[base['school'] == 'MS', 'age'].mode().values[0]
+st.write("A moda do consumo de álcool dos alunos da escola MS durante a semana de trabalho é:", moda_consumo_alcool_MS)
+
+
+
+
 
